5647c0c09e9227cfd9851716a0cede2b7a02c897
jaehyun jung <78546867+RePro1202@users.noreply.github.com>
2025-02-26 14:41:50 +0900

    [Bronze V] Title: 노트북 세 대를 가지고 왔다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33515. 노트북 세 대를 가지고 왔다/노트북 세 대를 가지고 왔다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t1, t2;
+    cin >> t1 >> t2;
+
+    cout << min(t1, t2);
+
+    return 0;
+}
