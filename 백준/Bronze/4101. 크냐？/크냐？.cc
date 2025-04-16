e92f652137b1d616ad3343b7464f1fc3698bf866
jaehyun jung <78546867+RePro1202@users.noreply.github.com>
2025-04-17 05:49:00 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    while (true)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a == 0 && b == 0)
+            break;
+
+        if (a > b)
+        {
+            cout << "Yes\n";
+        }
+        else
+        {
+            cout << "No\n";
+        }
+    }
+
+    return 0;
+}
